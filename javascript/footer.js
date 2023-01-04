3e2b3593dfdb3b05d699c82bef19bb11f286f56f
Luciano Adamczuk <lucianoadamczuk@gmail.com>
2023-01-04 11:00:38 -0300

    Javascript animations has been added

--- javascript/footer.js
+++ javascript/footer.js
@@ -8,55 +8,66 @@ const footerElement__7 = document.getElementById('footerElement__7')
 const footerElement__8 = document.getElementById('footerElement__8')
 const footerElement__9 = document.getElementById('footerElement__9')
 const footerElement__10 = document.getElementById('footerElement__10')
-const sumSign = document.querySelector('.sumSign')
-const footerMenu__1 = document.getElementById('footerMenu__1')
+
+const sumSign__1 = document.querySelector('.sumSign__1')
+const sumSign__2 = document.querySelector('.sumSign__2')
+const sumSign__3 = document.querySelector('.sumSign__3')
+const sumSign__4 = document.querySelector('.sumSign__4')
+const sumSign__5 = document.querySelector('.sumSign__5')
+const sumSign__6 = document.querySelector('.sumSign__6')
+const sumSign__7 = document.querySelector('.sumSign__7')
+const sumSign__8 = document.querySelector('.sumSign__8')
+const sumSign__9 = document.querySelector('.sumSign__9')
+const sumSign__10 = document.querySelector('.sumSign__10')
+
+
 
 footerElement__1.addEventListener('click', e =>{
     footerMenu__1.classList.toggle('showFooterMenu')
-    sumSign.classList.toggle('transformSumSign')
+    sumSign__1.classList.toggle('transformSumSign')
 })
 
 footerElement__2.addEventListener('click', e =>{
     footerMenu__2.classList.toggle('showFooterMenu')
-    sumSign.classList.toggle('transformSumSign')
+    sumSign__2.classList.toggle('transformSumSign')
 })
 
 footerElement__3.addEventListener('click', e =>{
     footerMenu__3.classList.toggle('showFooterMenu')
-    sumSign.classList.toggle('transformSumSign')
+    sumSign__3.classList.toggle('transformSumSign')
 })
 
 footerElement__4.addEventListener('click', e =>{
     footerMenu__4.classList.toggle('showFooterMenu')
-    sumSign.classList.toggle('transformSumSign')
+    sumSign__4.classList.toggle('transformSumSign')
 })
 
 footerElement__5.addEventListener('click', e =>{
     footerMenu__5.classList.toggle('showFooterMenu')
-    sumSign.classList.toggle('transformSumSign')
+    sumSign__5.classList.toggle('transformSumSign')
 })
 
 footerElement__6.addEventListener('click', e =>{
     footerMenu__6.classList.toggle('showFooterMenu')
-    sumSign.classList.toggle('transformSumSign')
+    sumSign__6.classList.toggle('transformSumSign')
 })
 
 footerElement__7.addEventListener('click', e =>{
     footerMenu__7.classList.toggle('showFooterMenu')
-    sumSign.classList.toggle('transformSumSign')
+    sumSign__7.classList.toggle('transformSumSign')
 })
 
 footerElement__8.addEventListener('click', e =>{
     footerMenu__8.classList.toggle('showFooterMenu')
-    sumSign.classList.toggle('transformSumSign')
+    sumSign__8.classList.toggle('transformSumSign')
 })
 
 footerElement__9.addEventListener('click', e =>{
     footerMenu__9.classList.toggle('showFooterMenu')
-    sumSign.classList.toggle('transformSumSign')
+    sumSign__9.classList.toggle('transformSumSign')
 })
 
 footerElement__10.addEventListener('click', e =>{
     footerMenu__10.classList.toggle('showFooterMenu')
-    sumSign.classList.toggle('transformSumSign')
+    sumSign__10.classList.toggle('transformSumSign')
 })
--- javascript/nav.js
+++ javascript/nav.js
@@ -1,17 +1,43 @@
 const hamburguerMenu = document.getElementById('hamburguerMenu')
 const nav = document.getElementById('nav')
 const navMenu = document.getElementById('navMenu')
+let navClosed = true
 const stick1 = document.getElementById('stick1')
 const stick2 = document.getElementById('stick2')
 
 
-
-hamburguerMenu.addEventListener('click', e => {
-    nav.classList.toggle('navToggle')
-    navMenu.classList.toggle('navMenuAddAnimation')
-    stick1.classList.toggle('hambuergerMenu__stick1-addAnimation')
-    stick2.classList.toggle('hambuergerMenu__stick2-addAnimation')
+hamburguerMenu.addEventListener('click', e => { 
+    if (navClosed == true){
+        nav.classList.add('navAnimation')
+        navMenu.classList.add('navMenuAddAnimation')
+        stick1.classList.add('hambuergerMenu__stick1-addAnimation')
+        stick2.classList.add('hambuergerMenu__stick2-addAnimation')
+        navClosed = false
+        
+        function removeNavCloserPropierties(){
+            navMenu.classList.remove('navMenuAddAnimation-closer')
+            stick1.classList.remove('hambuergerMenu__stick1-addAnimation-closer')
+            stick2.classList.remove('hambuergerMenu__stick2-addAnimation-closer')
+        }
+        removeNavCloserPropierties()
+    }
+    else{
+        navMenu.classList.add('navMenuAddAnimation-closer')
+        stick1.classList.add('hambuergerMenu__stick1-addAnimation-closer')
+        stick2.classList.add('hambuergerMenu__stick2-addAnimation-closer')
+        navClosed = true
+        
+        function removeNavOpenerPropierties(){
+            nav.classList.remove('navAnimation')
+            navMenu.classList.remove('navMenuAddAnimation')
+            stick1.classList.remove('hambuergerMenu__stick1-addAnimation')
+            stick2.classList.remove('hambuergerMenu__stick2-addAnimation')
+        }
+        setTimeout(removeNavOpenerPropierties, 280)
+    }
 }) 
+
+
 
 
 
